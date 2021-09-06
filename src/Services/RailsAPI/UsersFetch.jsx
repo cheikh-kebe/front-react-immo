c51315c6cee56ad4df37d91a594c366c08f8922f
Benoit Figea <60694673+BFH92@users.noreply.github.com>
2021-09-07 00:17:38 +0200

    formatting

--- src/Services/RailsAPI/UsersFetch.jsx
+++ src/Services/RailsAPI/UsersFetch.jsx
@@ -4,53 +4,53 @@ import axios from "axios";
 
 const API = axios.create({ baseURL: API_URL });
 
-const registerToken =(token)=>{
+const registerToken = (token) => {
   token = token.replace(/Bearer /g, "");
   Cookies.set("token", token);
+};
 
-}
-
-  const config= {
-    headers: {
-      "Content-Type": "application/json"
-    },
-  };
-
+const config = {
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
 
 export default class APIManager {
   static async register(email, password) {
-  
-    const response = await API.post("/users", { user: { email, password } }, config);
-  
+    const response = await API.post(
+      "/users",
+      { user: { email, password } },
+      config
+    );
+
     let token = await response.headers.authorization;
-    console.log(response)
-    
-    token? registerToken(token) : Cookies.set("isLogged?", "false")
-    this.login(email,password)
+    console.log(response);
+
+    token ? registerToken(token) : Cookies.set("isLogged?", "false");
+    this.login(email, password);
     return response;
-  
-  
   }
   static async logout() {
-    const authorizedConfig= {
+    const authorizedConfig = {
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${
-          Cookies.get("token")
-          }`
+        Authorization: `Bearer ${Cookies.get("token")}`,
       },
     };
     const response = await API.delete("/users/sign_out", authorizedConfig);
     Cookies.remove("token");
     return response;
   }
-  
-  static async login(email, password) {
 
-    const response = await API.post("/users/sign_in", {  user: { email, password } }, config);
+  static async login(email, password) {
+    const response = await API.post(
+      "/users/sign_in",
+      { user: { email, password } },
+      config
+    );
     let token = await response.headers.authorization;
-    console.log(response)
-    token? registerToken(token) : Cookies.set("isLogged?", "false")
+    console.log(response);
+    token ? registerToken(token) : Cookies.set("isLogged?", "false");
     return response;
   }
 }
