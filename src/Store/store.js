be3ee05c9a5bc1b5009c27ff316cc66c00e3f750
Benoit Figea <60694673+BFH92@users.noreply.github.com>
2021-09-07 12:17:54 +0200

    redux persist install

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,10 +1,22 @@
 import {createStore, combineReducers, applyMiddleware, compose} from 'redux'
 import LoggedUserReducer from './Reducers/LoggedUserReducer'
 import thunkMiddleware from 'redux-thunk'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2';
 
 
 const rootReducer = combineReducers({
   User: LoggedUserReducer
 })
 
-export let store = createStore(rootReducer, compose(applyMiddleware(thunkMiddleware),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: autoMergeLevel2,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export let store = createStore(persistedReducer, compose(applyMiddleware(thunkMiddleware),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+
+export let persistor = persistStore(store)
