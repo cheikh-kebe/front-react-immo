7843841d74a8fa6037bb54f1123320a5b6f5b940
VeraTuran <71694078+VeraTuran@users.noreply.github.com>
2021-09-07 19:39:18 +0200

    Revert "Fetch"

--- src/Config/fetchData.js
+++
@@ -1,9 +0,0 @@
-import { API_URL } from '../Config/config';
-
-export const fetchDataIndex = (setData, fetchUrl) => {
-  fetch(`${API_URL}${fetchUrl}`)
-    .then((response) => response.json())
-    .then((data) => {
-      setData(data);
-    });
-};
